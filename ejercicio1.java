7395833fbfa99cc1c6200dfb5a1760606a063151
JoseSembrera <143567473+JoseSembrera@users.noreply.github.com>
2023-09-02 20:08:45 -0500

    Update ejercicio1.java

--- ejercicio1.java
+++ ejercicio1.java
@@ -1,1 +1,132 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    if (6<6*5)
+	        System.out.println("Hello");
+	        System.out.println("There");
+	    
+	    // Respuesta: Hello
+	    // There
+	    
+	    /* 1 b */
+	    
+	    int x=1, y=2, z=3;
+	    
+	    if(x>y)
+	    if(x>z) 
+	       System.out.println("11111"); 
+	    else 
+	       System.out.println("22222"); 
+	  // Respuesta : no imprime nada 
+	  
+	    /**1 c*/
+	  
+	    int x=1, y=2, z=3;
+	    
+	    if (x<z)
+	      System.out.println("*");
+	    else if (x == z)
+	      System.out.println("&");
+	    else 
+	      System.out.println("$");
+	    //Respuesta : no imprime nada
+	    
+	    /**1 d*/
+	    
+	    int x=1, y=2, z=3;
+	    
+	    if (x<y)
+	      System.out.println("####");
+	    else
+	      System.out.println("&&&&");
+	      System.out.println("****");
+	    //Respuesta : no imprime nada
+	    
+	    /**1 e*/
+	    
+	    int x=1, y=2, z=3;
+	    
+	    if (x>y)
+	      System.out.println("####");
+	    else
+	      System.out.println("&&&&");
+	      System.out.println("****");
+	    }
+  	    //Respuesta : no imprime nada
 
+	    /**1 f*/
+	    
+	    int x=1, y=2, z=3;
+	    
+	    if (x>y)
+	      System.out.println("####");
+	    else {
+	      System.out.println("&&&&");
+	      System.out.println("****");
+	    }
+	    
+	    //Respuesta : no imprime nada
+	    
+	    /**1 g*/
+	    
+	    int a1=100; int a2=200;
+	    
+	    if(a1>100 && a2<=200)
+	      System.out.println(a1+" "+a2+" "+(a1+a2));
+	    else
+	      System.out.println(a1+" "+a2+" "+(2*a1-a2));
+	      
+	    //Respuesta : no imprime nada
+	    
+	    /**1 h*/
+	    int x=1, y=2, z=3;
+	    
+    if (++x > y++ // x-- > 0 )
+	      z++;
+	    else 
+	      z--;
+	      System.out.println(x+" "+y+" "+z);
+	      
+	    //Respuesta : no imprime nada
+	    
+	    /**1 i*/
+	    int x=1, y=2, z=3;
+	    
+	    if (x<y){
+	      System.out.println("####");
+	    }
+	    else
+	      System.out.println("****");
+	      System.out.println("&&&&");
+	    
+	    //Respuesta : no imprime nada
+	    
+	    /**1 j*/
+	    
+	    int x=1, y=2, z=3;
+	    
+	    if (´x´>´y´ // 66 > (int)(´A´))
+	      System.out.println("#*#");
+	      
+	    //Respuesta : no imprime nada
+	    
+	    /**1 k*/
+	    
+	    int x=1, y=2, z=3;
+	    
+	    if (x<y)
+	      System.out.println("*");
+	    else if (x == z)
+	      System.out.println("&");
+	    else
+	      System.out.println("$");	    
+	    
+	    //Respuesta : no imprime nada
+	    
+	    
+ 
+    }
+}
+  
+	      
